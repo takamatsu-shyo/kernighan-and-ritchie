59dc03ebe56baa3d32e21a89269ae15c481b9558
SHYO <takamatsu.shyo@gmail.com>
2023-01-06 17:09:05 +0200

    5-7_month-day

---
+++ ch5/5-7_mdim_array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+
+/* day_of_year: set day of year from month&day */
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+    {
+        yearday -= daytab[leap][i];
+    }
+    *pmonth = i;
+    *pday = yearday;
+
+}
+
+void main()
+{
+    printf("%i\n", day_of_year(2023,12,5));
+
+    int passed_month, passed_day;
+    month_day(2023, 165, &passed_month, &passed_day);
+    printf("%d %d\n", passed_month, passed_day);
+}
